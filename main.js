4b9ff7139298216d4716053c49b5698ec1b7f9da
Den <89767959+JrMzxc@users.noreply.github.com>
2022-04-13 18:03:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,172 @@
+var mysong = document.getElementById("mysong");
+var icon = document.getElementById("icon");
+
+icon.onclick = function () {
+    if(mysong.paused){
+        mysong.play();
+        icon.src = "./images/pause.png";
+
+    }else{
+        mysong.pause();
+        icon.src = "./images/play.png";
+    }
+}
+
+var mysong1 = document.getElementById("mysong1");
+var song1 = document.getElementById("song1");
+
+song1.onclick = function () {
+    if(mysong1.paused){
+        mysong1.play();
+        song1.src = "./images/pause.png";
+
+    }else{
+        mysong1.pause();
+        song1.src = "./images/play.png";
+    }
+}
+
+var mysong2 = document.getElementById("mysong2");
+var song2 = document.getElementById("song2");
+
+song2.onclick = function () {
+    if(mysong2.paused){
+        mysong2.play();
+        song2.src = "./images/pause.png";
+
+    }else{
+        mysong2.pause();
+        song2.src = "./images/play.png";
+    }
+}
+
+var mysong3 = document.getElementById("mysong3");
+var song3 = document.getElementById("song3");
+
+song3.onclick = function () {
+    if(mysong3.paused){
+        mysong3.play();
+        song3.src = "./images/pause.png";
+
+    }else{
+        mysong3.pause();
+        song3.src = "./images/play.png";
+    }
+}
+
+
+var mysong4 = document.getElementById("mysong4");
+var song4 = document.getElementById("song4");
+
+song4.onclick = function () {
+    if(mysong4.paused){
+        mysong4.play();
+        song4.src = "./images/pause.png";
+
+    }else{
+        mysong4.pause();
+        song4.src = "./images/play.png";
+    }
+}
+
+
+var mysong5 = document.getElementById("mysong5");
+var song5 = document.getElementById("song5");
+
+song5.onclick = function () {
+    if(mysong5.paused){
+        mysong5.play();
+        song5.src = "./images/pause.png";
+
+    }else{
+        mysong5.pause();
+        song5.src = "./images/play.png";
+    }
+}
+
+
+var mysong6 = document.getElementById("mysong6");
+var song6 = document.getElementById("song6");
+
+song6.onclick = function () {
+    if(mysong6.paused){
+        mysong6.play();
+        song6.src = "./images/pause.png";
+
+    }else{
+        mysong6.pause();
+        song6.src = "./images/play.png";
+    }
+}
+
+var mysong7 = document.getElementById("mysong7");
+var song7 = document.getElementById("song7");
+
+song7.onclick = function () {
+    if(mysong7.paused){
+        mysong7.play();
+        song7.src = "./images/pause.png";
+
+    }else{
+        mysong7.pause();
+        song7.src = "./images/play.png";
+    }
+}
+
+var mysong8 = document.getElementById("mysong8");
+var song8 = document.getElementById("song8");
+
+song8.onclick = function () {
+    if(mysong8.paused){
+        mysong8.play();
+        song8.src = "./images/pause.png";
+
+    }else{
+        mysong8.pause();
+        song8.src = "./images/play.png";
+    }
+}
+
+var mysong9 = document.getElementById("mysong9");
+var song9 = document.getElementById("song9");
+
+song9.onclick = function () {
+    if(mysong9.paused){
+        mysong9.play();
+        song9.src = "./images/pause.png";
+
+    }else{
+        mysong9.pause();
+        song9.src = "./images/play.png";
+    }
+}
+
+var mysong10 = document.getElementById("mysong10");
+var song10 = document.getElementById("song10");
+
+song10.onclick = function () {
+    if(mysong10.paused){
+        mysong10.play();
+        song10.src = "./images/pause.png";
+
+    }else{
+        mysong10.pause();
+        song10.src = "./images/play.png";
+    }
+}
+
+var mysong11 = document.getElementById("mysong11");
+var song11 = document.getElementById("song11");
+
+song11.onclick = function () {
+    if(mysong11.paused){
+        mysong11.play();
+        song11.src = "./images/pause.png";
+
+    }else{
+        mysong11.pause();
+        song11.src = "./images/play.png";
+    }
+}
+
+
